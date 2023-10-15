37502e2021c374be05361025fc8d38b005db3dfe
Guilherme Figueira <guibf2004@gmail.com>
2023-10-15 17:05:29 -0300

    sim

--- src/app/layouts/PageHeader.tsx
+++ src/app/layouts/PageHeader.tsx
@@ -2,7 +2,24 @@
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../../../public/Logo.png";
-import { Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
+import {
+	ArrowLeft,
+	Bell,
+	CircleDollarSign,
+	Contact2,
+	Languages,
+	LogOut,
+	Menu,
+	Mic,
+	MoonIcon,
+	Search,
+	Shield,
+	SunIcon,
+	Upload,
+	User,
+	Users,
+	Youtube,
+} from "lucide-react";
 import { Button } from "../components/ui/button";
 import { ModeToggle } from "../components/ui/theme-toggler";
 import { Input } from "../components/ui/input";
@@ -23,11 +40,19 @@ import {
 } from "@/app/components/ui/dropdown-menu";
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import { Skeleton } from "../components/ui/skeleton";
+import { useTheme } from "next-themes";
+import { Switch } from "../components/ui/switch";
 export default function PageHeader() {
+	const { theme, setTheme } = useTheme();
 	const [showFullWidthSearch, setShowFullWidthSearch] = useState(false);
+	const [strictMode, setStrictMode] = useState(false);
 	return (
 		<div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4 ">
-			<div className="flex gap-4 items-center flex-shrink-0">
+			<div
+				className={` gap-4 items-center flex-shrink-0 ${
+					showFullWidthSearch ? "hidden" : "flex"
+				}`}
+			>
 				<Button variant={"ghost"} size={"icon"}>
 					<Menu />
 				</Button>
@@ -41,9 +66,23 @@ export default function PageHeader() {
 						/>
 					</Link>
 				</Button>
-				<ModeToggle />
 			</div>
-			<form className="hidden md:flex gap-4 flex-grow justify-center">
+			<form
+				className={` gap-4 flex-grow justify-center ${
+					showFullWidthSearch ? "flex" : "hidden md:flex"
+				}`}
+			>
+				{showFullWidthSearch && (
+					<Button
+						type={"button"}
+						onClick={() => setShowFullWidthSearch(false)}
+						variant={"ghost"}
+						size={"icon"}
+						className="flex shrink-0"
+					>
+						<ArrowLeft />
+					</Button>
+				)}
 				<div className="flex flex-grow max-w-[37rem]">
 					<Input
 						placeholder="Pesquisar"
@@ -67,7 +106,7 @@ export default function PageHeader() {
 			</form>
 			<div
 				className={`flex md:gap-2 items-center flex-shrink-0 ${
-					showFullWidthSearch ? "" : ""
+					showFullWidthSearch ? "hidden" : "flex"
 				}`}
 			>
 				<Button
@@ -123,62 +162,115 @@ export default function PageHeader() {
 							</div>
 						</DropdownMenuLabel>
 						<DropdownMenuSeparator />
-						<DropdownMenuGroup className="px-4">
+						<DropdownMenuGroup>
 							<DropdownMenuItem>
 								<User className="mr-2 h-4 w-4" />
-								<span>Profile</span>
-								<DropdownMenuShortcut>⇧⌘P</DropdownMenuShortcut>
+								<span>Seu Canal</span>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
-								{/* <CreditCard className="mr-2 h-4 w-4" /> */}
-								<span>Billing</span>
-								<DropdownMenuShortcut>⌘B</DropdownMenuShortcut>
+								<Youtube className="mr-2 h-4 w-4" />
+								<span>Youtube Studio</span>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
-								{/* <Settings className="mr-2 h-4 w-4" /> */}
-								<span>Settings</span>
-								<DropdownMenuShortcut>⌘S</DropdownMenuShortcut>
+								<Users className="mr-2 h-4 w-4" />
+								<span>Alternar Conta</span>
 							</DropdownMenuItem>
 							<DropdownMenuItem>
-								{/* <Keyboard className="mr-2 h-4 w-4" /> */}
-								<span>Keyboard shortcuts</span>
-								<DropdownMenuShortcut>⌘K</DropdownMenuShortcut>
+								<LogOut className="mr-2 h-4 w-4" />
+								<span>Sair</span>
 							</DropdownMenuItem>
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
-						<DropdownMenuGroup className="px-4">
+						<DropdownMenuGroup>
 							<DropdownMenuItem>
-								{/* <Users className="mr-2 h-4 w-4" /> */}
-								<span>Team</span>
+								<CircleDollarSign className="mr-2 h-4 w-4" />
+								<span>Compras e Assinaturas</span>
 							</DropdownMenuItem>
+							<DropdownMenuItem>
+								<Contact2 className="mr-2 h-4 w-4" />
+								<span>Seus dados no Youtube</span>
+							</DropdownMenuItem>
+						</DropdownMenuGroup>
+						<DropdownMenuSeparator />
+						<DropdownMenuGroup>
 							<DropdownMenuSub>
 								<DropdownMenuSubTrigger>
-									{/* <UserPlus className="mr-2 h-4 w-4" /> */}
-									<span>Invite users</span>
+									<SunIcon className="mr-2 h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+									<MoonIcon className="absolute mr-2 h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+									<span>
+										Aparência:{" "}
+										<span className="capitalize">
+											{theme}
+										</span>
+									</span>
 								</DropdownMenuSubTrigger>
 								<DropdownMenuPortal>
-									<DropdownMenuSubContent>
-										<DropdownMenuItem>
-											{/* <Mail className="mr-2 h-4 w-4" /> */}
-											<span>Email</span>
+									<DropdownMenuSubContent className="bg-secondary">
+										<DropdownMenuItem
+											onClick={() => setTheme("light")}
+										>
+											Light
 										</DropdownMenuItem>
-										<DropdownMenuItem>
-											{/* <MessageSquare className="mr-2 h-4 w-4" /> */}
-											<span>Message</span>
+										<DropdownMenuItem
+											onClick={() => setTheme("dark")}
+										>
+											Dark
 										</DropdownMenuItem>
-										<DropdownMenuSeparator />
-										<DropdownMenuItem>
-											{/* <PlusCircle className="mr-2 h-4 w-4" /> */}
-											<span>More...</span>
+										<DropdownMenuItem
+											onClick={() => setTheme("system")}
+										>
+											System
 										</DropdownMenuItem>
 									</DropdownMenuSubContent>
 								</DropdownMenuPortal>
 							</DropdownMenuSub>
-							<DropdownMenuItem>
-								{/* <Plus className="mr-2 h-4 w-4" /> */}
-								<span>New Team</span>
-								<DropdownMenuShortcut>⌘+T</DropdownMenuShortcut>
-							</DropdownMenuItem>
+							<DropdownMenuSub>
+								<DropdownMenuSubTrigger>
+									<Languages className="mr-2 h-4 w-4" />
+									<span>Idioma</span>
+								</DropdownMenuSubTrigger>
+								<DropdownMenuPortal>
+									<DropdownMenuSubContent className="bg-secondary">
+										<DropdownMenuItem>
+											Português
+										</DropdownMenuItem>
+										<DropdownMenuItem>
+											English
+										</DropdownMenuItem>
+										<DropdownMenuItem>
+											System
+										</DropdownMenuItem>
+									</DropdownMenuSubContent>
+								</DropdownMenuPortal>
+							</DropdownMenuSub>
+							<DropdownMenuSub>
+								<DropdownMenuSubTrigger>
+									<Shield className="mr-2 h-4 w-4" />
+									<span>Modo restrito: {strictMode}</span>
+								</DropdownMenuSubTrigger>
+								<DropdownMenuPortal>
+									<DropdownMenuSubContent className="w-80 bg-secondary">
+										<h1 className="px-4 py-2">
+											Modo Restrito
+										</h1>
+										<DropdownMenuSeparator />
+										<p className="px-4 py-2 text-sm">
+											Essa opção ajuda a ocultar vídeos
+											que possam ter conteúdo para
+											maiores. Nenhum filtro é 100%
+											preciso. <br />
+											<br /> Esta configuração se aplica
+											apenas a este navegador.
+										</p>
+										<div className="flex items-center justify-between px-4 py-2 uppercase">
+											<h2 className="contrast-0 font-medium">
+												Ative o modo restrito
+											</h2>
+											<Switch />
+										</div>
+									</DropdownMenuSubContent>
+								</DropdownMenuPortal>
+							</DropdownMenuSub>
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
 						<DropdownMenuItem>
@@ -197,7 +289,6 @@ export default function PageHeader() {
 						<DropdownMenuItem>
 							{/* <LogOut className="mr-2 h-4 w-4" /> */}
 							<span>Log out</span>
-							<DropdownMenuShortcut>⇧⌘Q</DropdownMenuShortcut>
 						</DropdownMenuItem>
 					</DropdownMenuContent>
 				</DropdownMenu>
