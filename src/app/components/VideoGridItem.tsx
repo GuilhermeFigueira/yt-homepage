86810352342bd2913b2f3a2bdd599bfa8bcea6c9
Guilherme Figueira <guibf2004@gmail.com>
2023-10-23 16:00:17 -0300

    yes

--- src/app/components/VideoGridItem.tsx
+++ src/app/components/VideoGridItem.tsx
@@ -1,6 +1,9 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
+import { useEffect, useRef, useState } from "react";
 type VideoGridItemProps = {
 	id: string;
 	title: string;
@@ -16,6 +19,8 @@ type VideoGridItemProps = {
 	videoUrl: string;
 };
 
+const VIEW_FORMATTER = Intl.NumberFormat(undefined, { notation: "compact" });
+
 export default function VideoGridItem({
 	channel,
 	duration,
@@ -26,11 +31,31 @@ export default function VideoGridItem({
 	videoUrl,
 	views,
 }: VideoGridItemProps) {
+	const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+	const videoRef = useRef<HTMLVideoElement>(null);
+
+	useEffect(() => {
+		if (videoRef.current == null) return;
+
+		if (isVideoPlaying) {
+			videoRef.current.currentTime = 0;
+			videoRef.current.play();
+		} else {
+			videoRef.current.pause();
+		}
+	}, [isVideoPlaying]);
+
 	return (
-		<div className="flex flex-col gap-2">
+		<div
+			className="flex flex-col gap-2"
+			onMouseEnter={() => setIsVideoPlaying(true)}
+			onMouseLeave={() => setIsVideoPlaying(false)}
+		>
 			<Link href={`/watch?v=${id}`} className="relative aspect-video">
 				<Image
-					className="block w-full h-full object-cover rounded-xl"
+					className={`block w-full h-full object-cover transition-[border-radius] duration-200 ${
+						isVideoPlaying ? "rounded-none" : "rounded-xl"
+					}`}
 					src={thumbnailUrl}
 					alt={""}
 					width={1280}
@@ -39,6 +64,15 @@ export default function VideoGridItem({
 				<div className="absolute bottom-1 right-1 bg-secondary-foreground text-secondary text-sm px-0.5 rounded">
 					{formatDuration(duration)}
 				</div>
+				<video
+					ref={videoRef}
+					muted
+					playsInline
+					src={videoUrl}
+					className={`block h-full object-cover absolute delay-200 inset-0 transition-opacity duration-200 ${
+						isVideoPlaying ? "opacity-100" : "opacity-0"
+					}`}
+				/>
 			</Link>
 			<div className="flex gap-2">
 				<Link href={`/@${channel.id}`} className="flex-shrink-0 ">
@@ -54,8 +88,17 @@ export default function VideoGridItem({
 					<Link href={`/watch?v=${id}`} className="font-bold">
 						{title}
 					</Link>
-					<Link href={`/@${channel.id}`} className="text-primary/60">
+					<Link
+						href={`/@${channel.id}`}
+						className="text-primary/60 text-sm"
+					>
 						{channel.name}
+					</Link>
+					<Link href={`/watch?v=${id}`}>
+						<div className="text-primary/60 text-sm ">
+							{VIEW_FORMATTER.format(views)} Views •{" "}
+							{formatTimeAgo(postedAt)}
+						</div>
 					</Link>
 				</div>
 			</div>
